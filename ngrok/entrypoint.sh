a02c9b2a36b357259794182f4091e0f01c8c9872
Sasawat Iammatavee <i.sasawat@gmail.com>
2018-01-03 22:03:05 +0700

    - moved ngrok for workspace ssh back to ngrok apache instance

--- ngrok/entrypoint.sh
+++ ngrok/entrypoint.sh
@@ -1,75 +1,35 @@
 #!/bin/sh -e
 
-if [ -n "$@" ]; then
-  exec "$@"
-fi
-
-# Legacy compatible:
-if [ -z "$NGROK_PORT" ]; then
-  if [ -n "$HTTPS_PORT" ]; then
-    NGROK_PORT="$HTTPS_PORT"
-  elif [ -n "$HTTPS_PORT" ]; then
-    NGROK_PORT="$HTTP_PORT"
-  elif [ -n "$APP_PORT" ]; then
-    NGROK_PORT="$APP_PORT"
-  fi
-fi
-
-
-ARGS="ngrok"
-
-# Set the protocol.
-if [ "$NGROK_PROTOCOL" = "TCP" ]; then
-  ARGS="$ARGS tcp"
-else
-  ARGS="$ARGS http"
-  NGROK_PORT="${NGROK_PORT:-80}"
-fi
-
-# Set the authorization token.
-if [ -n "$NGROK_AUTH" ]; then
-  ARGS="$ARGS -authtoken=$NGROK_AUTH "
-fi
-
-# Set the subdomain or hostname, depending on which is set
-if [ -n "$NGROK_HOSTNAME" ] && [ -n "$NGROK_AUTH" ]; then
-  ARGS="$ARGS -hostname=$NGROK_HOSTNAME "
-elif [ -n "$NGROK_SUBDOMAIN" ] && [ -n "$NGROK_AUTH" ]; then
-  ARGS="$ARGS -subdomain=$NGROK_SUBDOMAIN "
-elif [ -n "$NGROK_HOSTNAME" ] || [ -n "$NGROK_SUBDOMAIN" ]; then
-  if [ -z "$NGROK_AUTH" ]; then
-    echo "You must specify an authentication token after registering at https://ngrok.com to use custom domains."
-    exit 1
-  fi
-fi
-
-# Set a custom region
-if [ -n "$NGROK_REGION" ]; then
-  ARGS="$ARGS -region=$NGROK_REGION "
-fi
-
-if [ -n "$NGROK_HEADER" ]; then
-  ARGS="$ARGS -host-header=$NGROK_HEADER "
-fi
+NGROK_PORT="${NGROK_PORT:-80}"
 
 if [ -n "$NGROK_USERNAME" ] && [ -n "$NGROK_PASSWORD" ] && [ -n "$NGROK_AUTH" ]; then
-  ARGS="$ARGS -auth=\"$NGROK_USERNAME:$NGROK_PASSWORD\" "
-elif [ -n "$NGROK_USERNAME" ] || [ -n "$NGROK_PASSWORD" ]; then
-  if [ -z "$NGROK_AUTH" ]; then
-    echo "You must specify a username, password, and Ngrok authentication token to use the custom HTTP authentication."
-    echo "Sign up for an authentication token at https://ngrok.com"
-    exit 1
-  fi
+  AUTH="\"$NGROK_USERNAME:$NGROK_PASSWORD\""
 fi
 
-ARGS="$ARGS -log stdout"
+cat >> /home/ngrok/.ngrok2/ngrok.yml << EOL
+authtoken: "${NGROK_AUTH}"
+region: ${NGROK_REGION}
+log: stdout
+tunnels:
+  http:
+    proto: http
+    addr: "http:${NGROK_PORT}"
+    auth: "${AUTH}"
+    subdomain: "${NGROK_SUBDOMAIN}"
+    hostname: "${NGROK_HOSTNAME}"
+    host_header: "${NGROK_HEADER}"
+EOL
+    
+if [ ${NGROK_SSH} = true ]; then
+  cat >> /home/ngrok/.ngrok2/ngrok.yml << EOL
+  ssh:
+    proto: tcp
+    addr: "workspace:${NGROK_SSH_PORT}"
+    remote_addr: "${NGROK_SSH_REMOTE_ADDR}"
+EOL
+fi 
 
-# Set the port.
-if [ -z "$NGROK_PORT" ]; then
-  echo "You must specify a NGROK_PORT to expose."
-  exit 1
-fi
-ARGS="$ARGS http:`echo $NGROK_PORT | sed 's|^tcp://||'`"
+ARGS="ngrok start --all"
 
 set -x
 exec $ARGS
